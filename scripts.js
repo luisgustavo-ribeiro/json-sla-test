e6019c56481b26192bd8bfa7c4aa912f22c5b5cb
Luis Gustavo Ribeiro <luisgustavo.ribeiro@trentim.com>
2019-11-29 17:54:03 -0300

    finalizacao

--- scripts.js
+++ scripts.js
@@ -5,165 +5,194 @@ let servico = [];
 let classificacao = [];
 let slaDesejado = [];
 
-let requisicao = document.querySelector('#form-requisicao');
-let formData = new FormData(requisicao);
+let drpArea = document.querySelector("#drpArea");
+let drpCategoria = document.querySelector("#drpCategoria");
+let drpServico = document.querySelector("#drpServico");
+let drpClassificacao = document.querySelector("#drpClassificacao");
 
-$(window).on('load', () => {
-    BuscarDados();
-});
+let areaSelecionada;
+let categoriaSelecionada;
+let servicoSelecionado;
+let classificacaoSelecionada;
+
+switch (document.readyState) {
+    case "loading":
+        BuscarDados();
+    case "interactive":
+        console.log(document.readyState);
+    case "complete":
+        break;
+}
 
 function BuscarDados() {
-    return new Promise((resolve, reject) => {
-        $.ajax({
-            url: "https://raw.githubusercontent.com/luisgustavo-ribeiro/json-sla-test/master/data.json",
-            type: "GET",
-            dataType: 'json',
-            success: data => {
-                itensSla = data;
-                SepararItensDeSelecao();
-            },
-            error: error => {
-                alert(JSON.stringify(error));
-            }
-        });
+    let url = "https://raw.githubusercontent.com/luisgustavo-ribeiro/json-sla-test/master/data.json";
 
-        resolve();
-    });
+    fetch(url)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            itensSla = data;
+            BuscarAreas();
+        })
+        .catch(err => {
+            alert("Ops, algo deu errado!")
+            console.log(err)
+        });
 }
 
-function SepararItensDeSelecao() {
-    return new Promise((resolve, reject) => {
-        itensSla.forEach(item => {
-            area.indexOf(item.Area) === -1 ? area.push(item.Area) : '';
-        });
-
-        resolve();
-        MontarDropdownArea();
+function BuscarAreas() {
+    itensSla.forEach(item => {
+        area.indexOf(item.Area) === -1 ? area.push(item.Area) : "";
     });
-}
 
-function MontarDropdownArea() {
     area.forEach(area => {
-        let option = "<option>" + area + "</option>";
-        $("#drpArea").append(option);
+        drpArea.options.add(new Option(area, ""));
     });
 
     BuscarCategorias();
-
-    $("#drpArea").change(() => {
-        categoria = [];
-        servico = [];
-        classificacao = [];
-        $("#drpCategoria option").remove();
-        $("#drpServico option").remove();
-        $("#drpClassificacao option").remove();
-        BuscarCategorias();
-    });
-
-    $("select").change(() => {
-        ExibirSLA();
-    });
 }
 
 function BuscarCategorias() {
+    areaSelecionada = drpArea.options[drpArea.selectedIndex].text;
+
     itensSla.forEach(item => {
-        if ($("#drpArea").val() === item.Area) {
+        if (areaSelecionada === item.Area) {
             categoria.indexOf(item.Categoria) === -1 ? categoria.push(item.Categoria) : "";
         }
     });
 
-    MontarDropdownCategoria();
-}
-
-function MontarDropdownCategoria() {
-    categoria.forEach(option => {
-        let optionItem = "<option>" + option + "</option>";
-        $("#drpCategoria").append(optionItem);
-    });
-
-    $("#drpCategoria").change(() => {
-        servico = [];
-        classificacao = [];
-        $("#drpServico option").remove();
-        $("#drpClassificacao option").remove();
-        BuscarServicos();
+    categoria.forEach(categoria => {
+        drpCategoria.options.add(new Option(categoria, ""));
     });
 
     BuscarServicos();
 }
 
 function BuscarServicos() {
+    categoriaSelecionada = drpCategoria.options[drpCategoria.selectedIndex].text;
+
     itensSla.forEach(item => {
-        if ($("#drpArea").val() === item.Area && $("#drpCategoria").val() === item.Categoria) {
+        if (areaSelecionada === item.Area && categoriaSelecionada === item.Categoria) {
             servico.indexOf(item.Servicos) === -1 ? servico.push(item.Servicos) : "";
         }
     });
 
-    MontarDropdownServicos();
-}
-
-function MontarDropdownServicos() {
-
-    servico.forEach(option => {
-        let optionItem = "<option>" + option + "</option>";
-        $("#drpServico").append(optionItem);
-    });
-
-    $("#drpServico").change(() => {
-        classificacao = [];
-        $("#drpClassificacao option").remove();
-        BuscarClassificacao();
+    servico.forEach(servico => {
+        drpServico.options.add(new Option(servico, ""));
     });
 
     BuscarClassificacao();
 }
 
 function BuscarClassificacao() {
+    servicoSelecionado = drpServico.options[drpServico.selectedIndex].text;
+
     itensSla.forEach(item => {
-        if ($("#drpArea").val() === item.Area && $("#drpCategoria").val() === item.Categoria && $("#drpServico").val() === item.Servicos) {
+        if (areaSelecionada === item.Area && categoriaSelecionada === item.Categoria && servicoSelecionado === item.Servicos) {
             classificacao.indexOf(item.Classificacao) === -1 ? classificacao.push(item.Classificacao) : "";
             slaDesejado.indexOf(item.SLA_DESEJADO) === -1 ? slaDesejado.push(item.SLA_DESEJADO) : "";
         }
     });
 
-    MontarDropdownClassificacao();
-}
-
-function MontarDropdownClassificacao() {
-    classificacao.forEach(option => {
-        let optionItem = "<option>" + option + "</option>";
-        $("#drpClassificacao").append(optionItem);
+    classificacao.forEach(classificacao => {
+        drpClassificacao.options.add(new Option(classificacao, ""));
     });
 
     ExibirSLA();
 }
 
 function ExibirSLA() {
-    $("#slaDesejado").text("");
+    classificacaoSelecionada = drpClassificacao.options[drpClassificacao.selectedIndex].text;
+    document.querySelector("#slaDesejado").innerHTML = "";
+    document.querySelector("#txtRequisito").value = "";
+
     itensSla.forEach(item => {
-        if ($("#drpArea").val() === item.Area && $("#drpCategoria").val() === item.Categoria && $("#drpServico").val() === item.Servicos && $("#drpClassificacao").val() === item.Classificacao) {
-            $("#slaDesejado").text(item.SLA_DESEJADO);
-            $("#txtRequisito").val(item.Requisitos);
+        if (areaSelecionada === item.Area && categoriaSelecionada === item.Categoria && servicoSelecionado === item.Servicos && classificacaoSelecionada === item.Classificacao) {
+            document.querySelector("#slaDesejado").innerHTML = item.SLA_DESEJADO;
+            document.querySelector("#txtRequisito").value = item.Requisitos;
         }
     });
 }
 
+drpArea.addEventListener('change', () => {
+    areaSelecionada = drpArea.options[drpArea.selectedIndex].text;
+
+    categoria = [];
+    servico = [];
+    classificacao = [];
+
+    drpCategoria.options.length = 0;
+    drpServico.options.length = 0;
+    drpClassificacao.options.length = 0;
+
+    BuscarCategorias();
+    ExibirSLA();
+});
+
+drpCategoria.addEventListener('change', () => {
+    categoriaSelecionada = drpCategoria.options[drpCategoria.selectedIndex].text;
+
+    servico = [];
+    classificacao = [];
+
+    drpServico.options.length = 0;
+    drpClassificacao.options.length = 0;
+
+    BuscarServicos();
+    ExibirSLA();
+});
+
+drpServico.addEventListener('change', () => {
+    servicoSelecionado = drpServico.options[drpServico.selectedIndex].text;
+
+    classificacao = [];
+
+    drpClassificacao.options.length = 0;
+
+    BuscarClassificacao();
+    ExibirSLA();
+});
+
+drpClassificacao.addEventListener('change', () => {
+    classificacaoSelecionada = drpClassificacao.options[drpClassificacao.selectedIndex].text;
+
+    ExibirSLA();
+});
+
 function EnviarRequisicao() {
 
-    let object = {};
+    if (document.querySelector("#solicitante").value != "") {
 
-    formData.forEach((value, key) => {
-        if (!object.hasOwnProperty(key)) {
-            object[key] = value;
-            return;
+        let form = {
+            Solicitante: document.querySelector("#solicitante").value,
+            Area: areaSelecionada,
+            Categoria: categoriaSelecionada,
+            Servico: servicoSelecionado,
+            Classificacao: classificacaoSelecionada,
+            Requisito: document.querySelector("#txtRequisito").value,
+            SLA: document.querySelector("#slaDesejado").innerText,
+            Descricao: document.querySelector("#txtDescricao").text            
+        };
+
+        try {
+            fetch('https://prod-89.westus.logic.azure.com:443/workflows/83d22548a7cb4844b4aae31147a08387/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=vMK6z2B4IjvSfYSFj7KO-nAPq_24O_CTObm9lZ6Ujtw', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(form)
+            });
+
+            alert("Solicitação enviada com sucesso!");
+
+        } catch (error) {
+            alert("Ops, algo deu errado! Tente novamente")
+            console.log(error);
         }
-        if (!Array.isArray(object[key])) {
-            object[key] = [object[key]];
-        }
-        object[key].push(value);
-    });
 
-    let json = JSON.stringify(object);
-
-    console.log(json);    
+    } else {
+        alert("Preencha seu email para enviar a solicitação")
+    }
 }
